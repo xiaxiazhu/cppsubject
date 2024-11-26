38c5008292d6ff97ce924a301128a4ad07c9c308
superstarwulin@126.com <superstarwulin@126.com>
2024-11-26 17:02:55 +0800

    完成众数的统计

--- cppSubject/main.cpp
+++ cppSubject/main.cpp
@@ -11,41 +11,48 @@
 #include <algorithm>
 #include <cmath>
 #include <climits>
+#include <unordered_map>
+
 using namespace std;
 
-
-
 int main() {
-        
-//    cout << "int 类型的最大值: " << INT_MAX << endl;
-//    cout << "long int 类型的最大值: " << LONG_MAX << endl;
-//    cout << "long long int 类型的最大值: " << LLONG_MAX << endl;
+
     int n = 0;
     cin>>n;
     
-    vector<int> faibonacciList;
+    vector<int> data;
     
-    // init
-    faibonacciList.push_back(0);
-    faibonacciList.push_back(1);
-    
-    for (int i = 2; i<=n; i++) {
-
-        int fnext = faibonacciList[i-1]%INT_MAX +faibonacciList[i-2]%INT_MAX;
-        
-        long int overflowValue = INT_MAX;
-        
-        if (INT_MAX-faibonacciList[i-1]<faibonacciList[i-2]){//两数相加的溢出判断
-            overflowValue =static_cast<long int>(faibonacciList[i-1])+static_cast<long int>(faibonacciList[i-2]);
-            fnext = overflowValue%INT_MAX;
-        }
-        
-        // fill i
-        faibonacciList.push_back(fnext);
-        
+    while (n>0) {
+        int temp = 0;
+        cin>>temp;
+        data.push_back(temp);
+        n--;
     }
     
-    cout<<faibonacciList[n];
+    unordered_map<int,int>freqMap;
+    int maxFreq = 0 ;
+    
+    vector<int> zhongshu;
+    
+    for (int num : data) {
+        freqMap[num]++;
+        maxFreq = max(maxFreq, freqMap[num]);
+    }
+    
+    for(auto &pair : freqMap){
+        if (pair.second==maxFreq) {
+            zhongshu.push_back(pair.first);
+        }
+    }
+    
+    // sort
+    sort(zhongshu.begin(), zhongshu.end());
+    
+    cout<<zhongshu.size()<<"\n";
+    
+    for (int j =0; j<zhongshu.size(); j++) {
+        cout<<zhongshu[j]<<" ";
+    }
     
     return 0;
 }
