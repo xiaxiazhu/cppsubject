75beea187b7c81fbc6375d611531030d3776804f
superstarwulin@126.com <superstarwulin@126.com>
2024-12-02 19:54:44 +0800

    某个数包含某位数

--- cppSubject/main.cpp
+++ cppSubject/main.cpp
@@ -16,38 +16,19 @@
 
 using namespace std;
 
-//个位数上的数字减去千位数上的数字，再减去百位数上的数字， 再减去十位数上的数字的结果大于等于零
-bool fuhe(int n){
-    vector<int> weishu;
-    while (n>0) {
-        weishu.push_back(n%10);
-        n=n/10;
-    }
-    //1234 1234 1349 6119 2123 5017
-//    reverse(weishu.begin(), weishu.end());
-    if (weishu[0]-weishu[3]-weishu[2]-weishu[1]>=0) {
-        return true;
+string check(long int n,long int d){
+    string nStr = to_string(n);
+    if (nStr.find(to_string(d))!=string::npos) {
+        return "true";
     }else{
-        return  false;
+        return "false";
     }
 }
 
 int main(){
-    int number;
-    cin>>number;
     
-    vector<int> nlist(number);
+    long int n,d;
+    cin>>n>>d;
     
-    for (int i =0; i<number; i++) {
-        cin>>nlist[i];
-    }
-    
-    int fuheSum=0;
-    for(int elem : nlist){
-        if(fuhe(elem)){
-            fuheSum++;
-        };
-    }
-    cout<<fuheSum;
-    return 1;
+    cout<<check(n,d);
 }
