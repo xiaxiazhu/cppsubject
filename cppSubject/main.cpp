ad6a2a8b2c69082d7bb08351141762767a809ba6
superstarwulin@126.com <superstarwulin@126.com>
2024-12-02 17:01:39 +0800

    素数的判断

--- cppSubject/main.cpp
+++ cppSubject/main.cpp
@@ -16,103 +16,39 @@
 
 using namespace std;
 
-int getValue(vector<vector<char>>mat,int x,int y){
-    
-//    leiquNum[x-1][y]
-    if (mat[x][y]=='0') {
-        return 0;
-    }
-    if(mat[x][y]=='?'){//no bomb
-        return 0;
-    }
-//    mat[x,y]
-    if(mat[x][y]=='*'){
-        return 1;
+bool isPrime(long int num){
+    //若一个正整数N为合数，则存在一个能整除N的数K，其中2<=K<=sqrt(N)
+    if (num==2) {
+        cout<<"YES";
+        return true;
     }
     
-    return 0;
+    if (num%2==0) {
+        cout<<"NO";
+        return false;
+    }
+    
+    if (num%3==0) {
+        return false;
+    }
+    
+    long int a = 1;
+    while (a*6-1<=sqrt(num)&&a>=1) {
+        
+        if(num%(a*6+1)==0||num%(a*6-1)==0){
+            cout<<"NO";
+            return false;
+        }
+        a++;
+    }
+    cout<<"YES";
+    return true;
 }
 
-int main() {
-    int n,m;
-    cin>>n>>m;
+int main(){
     
-    vector<vector<char>>leiqu(n,vector<char>(m));
-    
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            cin>> leiqu[i][j];
-        }
-    }
-    
-    //雷区数字输出
-    vector<vector<char>>leiquNum(n+2,vector<char>(m+2));
-    
-    int top=1,right=m+2,bottom=n+2,left=1;
-    // 四个点
-    // 四条边
-    for (int t = top; t<=right; t++) {
-        leiquNum[top-1][t-1] = '0';
-    }
-
-    for (int b = right; b>=left; b--) {
-        leiquNum[bottom-1][b-1] ='0';
-    }
-    
-    for (int r=top; r<=bottom; r++) {
-        leiquNum[r-1][right-1] = '0';
-    }
-
-    for (int l=bottom; l>=top; l--) {
-        leiquNum[l-1][left-1] = '0';
-    }
-    
-    // 8value
-    for (int a=0; a<n; a++) {
-        for (int c=0; c<m;c++) {
-            // 输入矩阵中，用向量加一输入
-            leiquNum[a+1][c+1]=leiqu[a][c];
-        }
-    }
-    
-//    for(auto aa:leiquNum){
-//        for(auto bb:aa){
-//            cout<<bb;
-//        }
-//        cout<<"\n";
-//    }
-
-    
-    for (int x=1; x<n+1; x++) {
-        for (int y=1; y<m+1; y++) {
-            
-            if(leiquNum[x][y]!='*'){
-                //search 8 direction
-                //上下左右等
-                leiquNum[x][y]= 48+(getValue(leiquNum,x-1,y)
-                                                  +getValue(leiquNum,x-1,y+1)
-                                                  +getValue(leiquNum,x,y+1)
-                                                  +getValue(leiquNum,x+1,y+1)
-                                                  +getValue(leiquNum,x+1,y)
-                                                  +getValue(leiquNum,x+1,y-1)
-                                                  +getValue(leiquNum,x,y-1)
-                                                  +getValue(leiquNum,x-1,y-1)
-                                                  );// ascii表，显示数字
-            }
-        }
-    }
-    
-    for (int d=0; d<n; d++) {
-        for (int e=0; e<m; e++) {
-            leiqu[d][e]=leiquNum[d+1][e+1];
-        }
-    }
-    
-    for(auto zz:leiqu){
-        for(auto vv:zz){
-            cout<<vv;
-        }
-        cout<<"\n";
-    }
+    long int num;
+    cin >>num;
+    isPrime(num);
     
 }
