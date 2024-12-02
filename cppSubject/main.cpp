af3f778e63b8034c6e744f498de224f9b287853f
superstarwulin@126.com <superstarwulin@126.com>
2024-12-02 16:22:52 +0800

    扫雷游戏

--- cppSubject/main.cpp
+++ cppSubject/main.cpp
@@ -16,49 +16,103 @@
 
 using namespace std;
 
+int getValue(vector<vector<char>>mat,int x,int y){
+    
+//    leiquNum[x-1][y]
+    if (mat[x][y]=='0') {
+        return 0;
+    }
+    if(mat[x][y]=='?'){//no bomb
+        return 0;
+    }
+//    mat[x,y]
+    if(mat[x][y]=='*'){
+        return 1;
+    }
+    
+    return 0;
+}
 
 int main() {
-    int n;
-    cin>>n;
+    int n,m;
+    cin>>n>>m;
     
-    vector<vector<int>> matrix(n,vector<int>(n,-1));
+    vector<vector<char>>leiqu(n,vector<char>(m));
     
-    int top = 1, right=n,bottom=n,left=1;
-    
-    int sum=0;
-    
-    while (top<=bottom && left<=right) {
-        
-        // 从左到右
-        for (int t=top; t<=right;t++) {
-            matrix[top-1][t-1] = ++sum;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin>> leiqu[i][j];
         }
-        top++;
-        
-        // 从上到下
-        for (int r= top; r<=bottom; r++) {
-            matrix[r-1][right-1]= ++sum;
-        }
-        right--;
-        
-        //从右到左
-        for (int b=right; b>=left; b--) {
-            matrix[bottom-1][b-1]=++sum;
-        }
-        bottom--;
-        
-        // 从下倒上
-        for (int l =bottom; l>=top; l--) {
-            matrix[l-1][left-1]= ++sum;
-        }
-        left++;
     }
-    for(auto m:matrix){
-        for(auto ma:m){
-            cout<<ma<<" ";
+    
+    //雷区数字输出
+    vector<vector<char>>leiquNum(n+2,vector<char>(m+2));
+    
+    int top=1,right=m+2,bottom=n+2,left=1;
+    // 四个点
+    // 四条边
+    for (int t = top; t<=right; t++) {
+        leiquNum[top-1][t-1] = '0';
+    }
+
+    for (int b = right; b>=left; b--) {
+        leiquNum[bottom-1][b-1] ='0';
+    }
+    
+    for (int r=top; r<=bottom; r++) {
+        leiquNum[r-1][right-1] = '0';
+    }
+
+    for (int l=bottom; l>=top; l--) {
+        leiquNum[l-1][left-1] = '0';
+    }
+    
+    // 8value
+    for (int a=0; a<n; a++) {
+        for (int c=0; c<m;c++) {
+            // 输入矩阵中，用向量加一输入
+            leiquNum[a+1][c+1]=leiqu[a][c];
+        }
+    }
+    
+//    for(auto aa:leiquNum){
+//        for(auto bb:aa){
+//            cout<<bb;
+//        }
+//        cout<<"\n";
+//    }
+
+    
+    for (int x=1; x<n+1; x++) {
+        for (int y=1; y<m+1; y++) {
+            
+            if(leiquNum[x][y]!='*'){
+                //search 8 direction
+                //上下左右等
+                leiquNum[x][y]= 48+(getValue(leiquNum,x-1,y)
+                                                  +getValue(leiquNum,x-1,y+1)
+                                                  +getValue(leiquNum,x,y+1)
+                                                  +getValue(leiquNum,x+1,y+1)
+                                                  +getValue(leiquNum,x+1,y)
+                                                  +getValue(leiquNum,x+1,y-1)
+                                                  +getValue(leiquNum,x,y-1)
+                                                  +getValue(leiquNum,x-1,y-1)
+                                                  );// ascii表，显示数字
+            }
+        }
+    }
+    
+    for (int d=0; d<n; d++) {
+        for (int e=0; e<m; e++) {
+            leiqu[d][e]=leiquNum[d+1][e+1];
+        }
+    }
+    
+    for(auto zz:leiqu){
+        for(auto vv:zz){
+            cout<<vv;
         }
         cout<<"\n";
     }
-
     
 }
