a86576dfd358c79ec8e174187ae5d77cab14821d
superstarwulin@126.com <superstarwulin@126.com>
2024-12-04 10:45:40 +0800

    输出某个范围内的素数对，相差为2叫做素数对

--- cppSubject/main.cpp
+++ cppSubject/main.cpp
@@ -16,48 +16,38 @@
 
 using namespace std;
 
-int digtalSum(int n){
-    
-    int sum=0;
-
-    while (n>0) {
-        int temp = n%10;
-        sum+=temp;
-        n=n/10;
-    }
-    return sum;
-}
-
-int findDigitalRoots(int num){
-    
-    int dr=0;
-    dr=digtalSum(num);
-    while (dr>=10) {
-        dr = digtalSum(dr);
-    }
-    
-    return dr;
-}
-
 int main(){
-    //一个整数的“Digital Roots”就是这个整数的各位数字的和，如果这个和是一个一位数，则这个一位数就是这个整数的“Digital Roots”，否则的话则继续求这个和的“Digital Roots”，直到最后得出一个一位数。
-
-    vector<int> numList;
+    //两个相差为2的素数称为素数对，如5和7，17和19等，要求找出给定范围内所有的素数对。
+    int min,max;
+    cin>>min>>max;
     
-    while (true) {
-        int x;
-        cin>>x;
-        if (x!=0) {
-            numList.push_back(x);
-        }else{break;}
+    // 找在primevector中的出素数
+    //埃拉托斯特尼筛法
+    if (min>=max) {
+        cout<<"empty";
+        return -1;
+    }
+    vector<bool> isPrime(max+1,true);
+    
+    isPrime[0]=false;isPrime[1]=false;
+    
+    for (int i=2; i*i<=max; i++) {
+        // 2 以及2的倍数不是prime
+        //isPrime[2]=true;
+        if (isPrime[i]) {
+            for (int j = i*i; j<=max; j+=i) {
+                isPrime[j]=false;
+            }
+        }
     }
     
-    for (int i=0; i<numList.size(); i++) {
-        
-        int result = findDigitalRoots(numList[i]);
-        cout<<result<<"\n";
-
-        
-    }
+    // 输出相差为2的素数对
     
+    for (int p=min; p<isPrime.size(); p++) {
+        if (isPrime[p]) {
+            if (isPrime[p+2]) {
+                cout<< p <<" " <<p+2<<"\n";
+            }
+        }
+    }
 }
