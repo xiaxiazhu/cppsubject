d04362ec788a73fb0687cd66956a66d95c1c061c
superstarwulin@126.com <superstarwulin@126.com>
2024-12-07 18:48:55 +0800

    彗星飞船的游戏

--- cppSubject/main.cpp
+++ cppSubject/main.cpp
@@ -20,16 +20,44 @@
 
 using namespace std;
 
+int searchTableIndex(char c){
+    
+    char table[27]=
+    {'^','A','B','C','D','E','F','G','H','I','G','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    
+    for (int i =0; i<sizeof(table)/sizeof(table[0]); i++) {
+        
+        if (table[i] == c) {
+            return i;
+        }
+    }
+    
+    return -1;
+}
+
 int main(){
         
-    string inputStr;
-    getline(cin, inputStr);
+    string HuixinName,xiaozuName;
     
-    transform(inputStr.begin(), inputStr.end(), inputStr.begin(), [](unsigned char c){
-        return toupper(c);
-    });
+    cin>>HuixinName;
+    cin>>xiaozuName;
     
-    cout<<inputStr;
+    int hxnNum=1,xzNum=1;
+    
+    for(char c : HuixinName){
+        hxnNum *= searchTableIndex(c);
+    }
+//    searchTableIndex("A")*
+    
+    for(char c: xiaozuName){
+        xzNum*= searchTableIndex(c);
+    }
+    
+    if (hxnNum%47 == xzNum%47) {
+        cout<<"GO";
+    }else{
+        cout<<"STAY";
+    }
     
     return 0;
 }
