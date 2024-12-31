e0cdb4d4f36bebe23fa88cbf58684f42702bf13f
superstarwulin@126.com <superstarwulin@126.com>
2024-12-31 09:37:04 +0800

    使用指针逆序输出一个数组

--- main4/main.cpp
+++ main4/main.cpp
@@ -8,30 +8,22 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *a,int *b){
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
 
 int main(int argc, const char * argv[]) {
-
-    int a,b;
     
-    cin>>a>>b;
+    int n;
+    cin>>n;
+    //16704 31898 29814 2071 12487 19559 14120 503 29835 1848 9637 17909 13680 27695 5395 23792 32745 4600 8235 26475 11630 13585 6800 29867 2111286
     
-    int * apt = &a;
+    int * arrPoint = new int(n);
     
-//    cout<<apt<<"\n";
+    for (int i=0; i<n; i++) {
+        cin >> *(arrPoint+i);
+    }
     
-    swap(&a,&b);
-    
-    cout<<a<<" "<<b<<"\n";
-    
-    cout<<"a address"<<"=="<<&a<<" "<<apt<<"\n";
-    
-    cout<<"a value"<<"=="<<*apt<<"\n";
+    for (int j=n-1; j>=0; j--) {
+        cout<< *(arrPoint+j)<<" ";
+    }
     
     return 0;
 }
