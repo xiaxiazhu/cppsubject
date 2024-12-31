fca1836ee6d205aaf2638b0b3573a91cd1a4653f
superstarwulin@126.com <superstarwulin@126.com>
2024-12-31 20:18:35 +0800

    插入排序

---
+++ sort/main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  sort
+//
+//  Created by zhuzhuxia on 2024/12/31.
+//
+
+#include <iostream>
+using namespace std;
+
+bool insertSort(int* arr,int n){
+        
+    for (int i=1; i<n; i++) {
+        
+        int j = i-1;
+        int key = arr[i];
+        
+        while (j>=0 && arr[j]<key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        
+        arr[j+1] = key;
+    }
+    
+    return 0;
+}
+
+int main() {
+    
+    int n;
+    cin>>n;
+    
+    int * arr = new int(n);
+    
+    for (int i=0; i<n; i++) {
+        cin >>*(arr+i);
+    }
+    
+    insertSort(arr,n);
+      
+    for (int a=0; a<n; a++) {
+        cout<<arr[a]<<" ";
+    }
+    
+    return 0;
+}
